15d20e8e1cf1d12f03706bcb7727b21c2d78dbc8
Luiz Carneiro <lhscarneiro@gmail.com>
2026-06-30 12:22:06 -0300

    criacao da estrutura inicial do projeto

---
+++ main.py
@@ -0,0 +1,19 @@
+# menu de opcoes para os operadores selecionaveis
+def mostar_menu():
+    print("===================")
+    print("    CALCULADORA    ")
+    print("===================")
+    print("\n")
+    print("1 - Soma")
+    print("2 - Subtracao")
+    print("3 - Multiplicacao")
+    print("4 - Divisao")
+    print("0 - SAIR")
+        
+def main():
+    mostar_menu()
+    
+
+if __name__ == "__main__":
+    main()
+    
